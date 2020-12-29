4497a321201cd0a115c2e3125cfb1316f149e0c0
kimchang0 <newshfkfk@gmail.com>
2020-12-30 01:07:27 +0900

    201230PythonStart

---
+++ practice.py
@@ -0,0 +1,68 @@
+
+# 숫자 자료형
+#print(5)
+#print(-10)
+#print(3.14)
+#print(1000)
+#print(5+3)
+#print(2*8)
+#print(3*(3+1))
+
+# 문자 자료형
+#print('풍선')
+#print("나비")
+#print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+#print("ㅋ"*9)
+
+# 불린 자료형
+#print(5>10)
+#print(5<10)
+#print(True)
+#print(False)
+#print(not True)
+#print(not False)
+#print(not (5 > 10))
+
+# 변수
+# 변수는 코딩을 간결하게 하고, 누구나 자신에게 맞게 내용을 변경할 수 있어서 편리하다.
+# animal = "강아지"
+# name  = "연탄이"
+#age = 4
+#hobby = "산책"
+#is_adult = age >= 3
+
+#print("우리집 " + animal + "의 이름은 " + name + "예요.")
+#print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요.")
+#print(name + "는 어른일까요? " + str(is_adult))
+
+#animal = "고양이"
+#name  = "해피"
+#age = 4
+#hobby = "낮잠"
+#is_adult = age >= 3
+
+# print("우리집 " + animal + "의 이름은 " + name + "예요.")
+# 위에 변수를 선언했지만 밑에서 재선언하면 재선언한 내용이 출력된다.
+# hobby = "공놀이"
+# print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요.")
+# +와 ,로 문장을 더할 수 있고 +는 그냥 더해지지만 ,는 한칸을 자동으로 띄고 더해진다.
+# print(name, "는 어른일까요? ", is_adult)
+
+#퀴즈
+# station = "사당"
+
+# print(station + "행 열차가 들어오고 있습니다.")
+
+# station = "신도림"
+
+# print(station + "행 열차가 들어오고 있습니다.")
+
+# station = "인천공항"
+
+# print(station, "행 열차가 들어오고 있습니다.")
+
+
+
+
+
+
