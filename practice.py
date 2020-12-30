bf71f7ee623d9453a34f1481b962c127021993d9
kimchang0 <newshfkfk@gmail.com>
2020-12-31 00:41:42 +0900

    201231_study

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
 
+'''201230'''
 # 숫자 자료형
 #print(5)
 #print(-10)
@@ -45,7 +46,7 @@
 # 위에 변수를 선언했지만 밑에서 재선언하면 재선언한 내용이 출력된다.
 # hobby = "공놀이"
 # print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요.")
-# +와 ,로 문장을 더할 수 있고 +는 그냥 더해지지만 ,는 한칸을 자동으로 띄고 더해진다.
+# +와 ,로 문장을 더할 수 있고 +는 그냥 더해지지만 ,는 한칸을 자동으로 띄고 더해진다. 또한 + 는 정수나 불린 값을 출력히기 위해 str()로 감싸줘야 하지만 ,는 그냥 출력 가능하다.
 # print(name, "는 어른일까요? ", is_adult)
 
 #퀴즈
@@ -60,6 +61,129 @@
 # station = "인천공항"
 
 # print(station, "행 열차가 들어오고 있습니다.")
+
+'''201231'''
+
+#연산자
+# print(1+1) # 2
+# print(3-2) # 1
+# print(5*2) # 10
+# print(6/3) # 2
+
+# print(2**3) # 2^3 = 8
+# print(5%3) # 나머지 2
+# print(10%3) # 나머지 1
+# print(5//3) # 몫
+# print(10//3)
+
+# print(10 > 3) # true
+# print(4 >= 7) # false
+# print(10 < 3) # false
+# print(5 <= 5) # true
+
+# print(3 == 3) # 같다 true
+# print(4 == 2) # false
+# print(3 + 4 == 7) # true
+
+# print(1 != 3) # 같지 않다 true
+# print(not(1 != 3)) # 1은 3과 같지 않기 때문에 트루지만 not을 붙여서 false가 나옴
+
+# print((3 > 0) and (3 < 5)) #true 동시조건은 and를 사용하거나 &를 하나 붙여서 표현함. 동시에 만족하면 ture가 나옴
+# print((3 > 0 ) & (3 < 5)) # ture 하나라도 만족하지 않으면 false
+
+# print((3 > 0) or (3 > 5)) # ture or조건은 or을 사용하거나 |를 사용해 표현함. 하나라도 만족하면 true가 나옴
+# print((3 > 0) | (3 > 5)) # true 둘 다 만족하지 않으면 false
+
+# print(5 > 4 > 3) # true 조건을 모두 만족하기 때문에 true
+# print(5 > 4 > 7) # false 조건 하나가 맞지 않기때문에 false
+
+# 수식
+# print(2 + 3 * 4)
+# print((2 + 3) * 4)
+
+# number = 2 + 3 * 4
+# print(number)
+
+# number = number + 2
+# print(number)
+
+# number += 2
+# print(number)
+
+# number *= 2
+# print(number)
+
+# number /= 2
+# print(number)
+
+# number -= 2
+# print(number)
+
+# number %= 2
+# print(number)
+
+# #숫자처리 함수
+# print(abs(-5)) # 절댓값 absolute
+# print(pow(4, 2)) # 제곱 power 4^2
+# print(max(5, 12)) #최대값 maximum 둘 중 큰 값을 찾아 반환
+# print(min(5, 12)) #최소값 minimum 둘 중 작은 값을 찾아 반환
+# print(round(3.14)) # 반올림 round
+# print(round(4.99))
+
+# from math import *
+# print(floor(4.99)) # 내림 floor
+# print(ceil(3.14)) # 올림 ceiling
+# print(sqrt(16)) # 제곱근 square root pram의 제곱근을 구함
+
+# 랜덤 함수
+# from random import *
+
+# print(random()) # 0.0 이상 1.0 이하의 임의의 값 생성
+# print(random() * 10) # 0.0 이상 10.0 이하의 임의의 값 생성
+# print(int(random() * 10)) # 0 이상 10 이하의 임의의 정수값 생성
+# print(int(random() * 10)) # 0 이상 10 이하의 임의의 정수값 생성
+# print(int(random() * 10)) # 0 이상 10 이하의 임의의 정수값 생성
+# print(int(random() * 45) + 1)# 1 이상 45 이하의 임의의 정수값 생성
+# print(int(random() * 45) + 1)# 1 이상 10 이하의 임의의 정수값 생성
+# print(int(random() * 45) + 1)# 1 이상 10 이하의 임의의 정수값 생성
+# print(int(random() * 45) + 1)# 1 이상 10 이하의 임의의 정수값 생성
+# print(int(random() * 45) + 1)# 1 이상 10 이하의 임의의 정수값 생성
+# print(int(random() * 45) + 1)# 1 이상 10 이하의 임의의 정수값 생성
+
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성 자동으로 1, 45 포함함.
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성 자동으로 1, 45 포함함.
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성 자동으로 1, 45 포함함.
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성 자동으로 1, 45 포함함.
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성 자동으로 1, 45 포함함.
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성 자동으로 1, 45 포함함.
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성 자동으로 1, 45 포함함.
+
+# 퀴즈
+
+''' Quiz) 당신은 최근에 코딩 스터디 모임을 새로 만들었습니다.
+월 4회 스터디를 하는데, 3번은 온라인으로 하고, 1번은 오프라인으로 하기로 했습니다.
+아래 조건에 맞는 오프라인 모임 날짜를 정해주는 프로그램을 작성하시오.
+
+조건1 : 랜덤으로 날짜를 뽑아야 함
+조건2 : 월별 날짜는 다름을 감안하여 최소 일수인 28 이내로 정함
+조건3 : 매월 1 ~ 3일은 스터디 준비를 해야 하므로 제외함
+
+(출력문 예제)
+오프라인 스터디 모임 날짜는 매월 x 일로 선정되었습니다.'''
+# from random import *
+# date = randint(4, 28)
+
+# print("오프라인 스터디 모임 날짜는 매월", date, "일로 선정되었습니다.")
+
+
 
 
 
