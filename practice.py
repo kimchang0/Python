3ba1a0aa3e86f4dddcea52875c9128b0bf0a3a14
kimchang0 <newshfkfk@gmail.com>
2021-01-04 23:50:21 +0900

    210104_study

--- practice.py
+++ practice.py
@@ -430,10 +430,13 @@
 
 from random import *
 
+id = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"}
 
-
-
-
+chicken = sample(id, 1)
+print(chicken)
+chicken = set(chicken)
+id = id.remove(chicken)
+print(id)
 
 
 
