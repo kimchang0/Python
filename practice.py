3dc0aceda6c1fc5e1dfcfa2fa9c6d18ea8051c73
kimchang0 <newshfkfk@gmail.com>
2021-01-06 23:54:48 +0900

    210106_study

--- practice.py
+++ practice.py
@@ -430,13 +430,15 @@
 
 from random import *
 
-id = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"}
+id = range(1, 21)
+id = list(id)
+shuffle(id)
+pick = sample(id, 4)
 
-chicken = sample(id, 1)
-print(chicken)
-print(id)
-
-
+print("-- 당첨자 발표--")
+print("치킨 당첨자 : {0}".format(pick[0]))
+print("커피 당첨자 : {0}".format(pick[1:]))
+print("-- 축하합니다 --")
 
 
 
