1721fd78d79287b41030bce77bba1888ac7553a6
kimchang0 <newshfkfk@gmail.com>
2021-01-20 23:54:52 +0900

    211020_study

--- practice.py
+++ practice.py
@@ -704,45 +704,117 @@
 # weight = round(std_weight(height / 100, gender), 2)
 # print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight)) 
 
+#표준입출력
+# print("Python", "Java", sep=",")
+# print("Python", "Java", "Javascript", sep=" vs ")
+# print("Python", "Java", sep=",", end="?")
+# print("무엇이 더 재밌을까요?")
 
+# import sys
+# print("Python", "java", file=sys.stdout)
+# print("Python", "java", file=sys.stderr)
 
+# scores = {"수학":0, "영어":50, "코딩":100}
+# for subject, score in scores.items():
+#     # print(subject, score)
+#     print(subject.ljust(3), str(score).rjust(4), sep=":")
 
+# for num in range(1, 21):
+#     # print("대기번호 : " + str(num))
+#     print("대기번호 : " + str(num).zfill(3))
 
+# answer = input("아무 값이나 입력하세요 : ")
+# print(type(answer))
+# print("입력하신 값은 " + answer + "입니다.")
 
+# 다양한 출력포멧
+# 빈 자리는 빈 공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+# print("{0: >10}".format(500))
 
+# # 양수일 때는 +로 표시, 음수일 때는 -로 표시
+# print("{0: >+10}".format(500))
+# print("{0: >+10}".format(-500))
 
+# # 왼쪽 정렬하고, 빈칸을 _로 채움
+# print("{0:_<+10}".format(500))
+# print("{0:_<10}".format(500))
 
+# # 3자리 마다 ,찍기
+# print("{0:,}".format(1000000000000))
 
+# # 3자리 마다 ,찍기 +,- 부호도 붙이기
+# print("{0:+,}".format(1000000000000))
+# print("{0:+,}".format(-1000000000000))
 
+# # 3자리 마다 ,찍기 +,- 부호도 붙이고 자리도 확보하고 빈 자리는 ^채우기
+# print("{0:^<+30,}".format(1000000000000))
 
+# # 소수점 출력
+# print("{0:f}".format(5/3))
 
+# # 소수점 특정 자리까지만 출력(소수점 3째 자리에서 반올림)
+# print("{0:.2f}".format(5/3))
 
+# 파일 입출력
+# score_file = open("score.txt", "w", encoding="utf8")
+# print("수학 : 0", file=score_file)
+# print("영어 : 50", file=score_file)
+# score_file.close()
 
+# score_file = open("score.txt", "a", encoding="utf8")
+# score_file.write("과학 : 80")
+# score_file.write("\n코딩 : 100")
+# score_file.close()
 
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.read())
+# score_file.close()
 
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# score_file.close()
 
+# score_file = open("score.txt", "r", encoding="utf8")
+# while True:
+#     line = score_file.readline()
+#     if not line:
+#         break
+#     print(line, end="")
+# score_file.close()
 
+# score_file = open("score.txt", "r", encoding="utf8")
+# lines = score_file.readlines()
+# for line in lines:
+#     print(line, end="")
 
+# score_file.close()
 
+# pickle
+# import pickle
+# profile_file = open("profile.pickle", "wb")
+# profile = {"이름":"박명수", "나이":30, "취미":["축구", "골프", "코딩"]}
+# print(profile)
+# pickle.dump(profile, profile_file) #프로필에 있는 정보를 파일에 저장
+# profile_file.close()
 
+# profile_file = open("profile.pickle", "rb")
+# profile = pickle.load(profile_file) #file에 있는 정보를 profile에 불러오기
+# print(profile)
+# profile_file.close()
 
+# with
+# import pickle
 
+# with open("profile.pickle", "rb") as profile_file:
+#     print(pickle.load(profile_file))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+with open("study.txt", "w", encoding="utf8") as study_file:
+    study_file.write("파이썬을 열심히 공부하고 있어요.")
+with open("study.txt", "r", encoding="utf8") as study_file:
+    print(study_file.read())
 
 
 
