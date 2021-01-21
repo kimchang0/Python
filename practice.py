2e40be560ab0546547f472becbf72305c3cf5638
kimchang0 <newshfkfk@gmail.com>
2021-01-21 23:44:05 +0900

    210121_study

--- practice.py
+++ practice.py
@@ -703,6 +703,7 @@
 # gender = "남자"
 # weight = round(std_weight(height / 100, gender), 2)
 # print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight)) 
+'''210120'''
 
 #표준입출력
 # print("Python", "Java", sep=",")
@@ -811,56 +812,166 @@
 # with open("profile.pickle", "rb") as profile_file:
 #     print(pickle.load(profile_file))
 
-with open("study.txt", "w", encoding="utf8") as study_file:
-    study_file.write("파이썬을 열심히 공부하고 있어요.")
-with open("study.txt", "r", encoding="utf8") as study_file:
-    print(study_file.read())
+# with open("study.txt", "w", encoding="utf8") as study_file:
+#     study_file.write("파이썬을 열심히 공부하고 있어요.")
+# with open("study.txt", "r", encoding="utf8") as study_file:
+#     print(study_file.read())
 
+''' Quiz) 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있습니다.
+보고서는 항상 아래와 같은 형태로 출력되어야 합니다.
 
+- X 주차 주간보고 - 
+부서 :
+이름 :
+업무 요약 :
 
+1주차부터 50주차까지의 보고서 파일을 만드는 프로그램을 작성하시오.
 
+조건 : 파일명은 "1주차.txt", "2주차.txt", ... 와 같이 만듭니다.'''
+# 내 풀이
+# i = 1
+# while i <= 50:
+#     with open(str(i) +"주차.txt", "w", encoding="utf8") as study_file:
+#         study_file.write("-" + str(i) + "주차 주간보고 -")
+#         study_file.write("\n부서 : ")
+#         study_file.write("\n이름 : ")
+#         study_file.write("\n업무 요약 : ")
+#     i += 1
 
+# 강의 풀이
+# for i in range(1, 51):
+#     with open(str(i) + "주차.txt", "w", encoding="utf8") as report_file:
+#         report_file.write("-" + str(i) + "주차 주간보고 -")
+#         report_file.write("\n부서 : ")
+#         report_file.write("\n이름 : ")
+#         report_file.write("\n업무 요약 : ")
+'''210120'''
 
+# 클래스
+# name = "마린"
+# hp = 40
+# damage = 5
 
+# print("{0} 유닛이 생성되었습니다.".format(name))
+# print("체력 {0}, 공격력{1}\n".format(hp, damage))
 
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 35
+# print("{0} 유닛이 생성되었습니다.".format(tank_name))
+# print("체력 {0}, 공격력{1}\n".format(tank_hp, tank_damage))
 
+# tank_name2 = "탱크"
+# tank_hp2 = 150
+# tank_damage2 = 35
+# print("{0} 유닛이 생성되었습니다.".format(tank_name))
+# print("체력 {0}, 공격력{1}\n".format(tank_hp, tank_damage))
 
+# def attack(name, location, damage):
+#     print("{0} : {1} 방향으로 적군을 공격합니다. [공격력{2}]".format(name, location, damage))
 
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+# attack(tank_name2, "1시", tank_damage2)
 
+# class Unit:
+#     def __init__(self, name, hp, damage):
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+#         print("{0} 유닛이 생성 되었습니다.".format(self.name))
+#         print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
 
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 35)
 
+# __init__(파이썬에서 쓰이는 생성자)
+# 객체를 생성했을 때 함수에서 쓰이는 파라미터를 전부 채워야 함. 일부를 빼면 실행이 되지 않음
 
+# 멤버변수
+# 클래스 내에서 정의된 변수를 멤버변수라고 함.
+# class Unit:
+#     def __init__(self, name, hp, damage):
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+#         print("{0} 유닛이 생성 되었습니다.".format(self.name))
+#         print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
 
+# wraith1 = Unit("레이스", 80, 5)
+# print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
 
+# wraith2 = Unit("레이스", 80, 5)
+# wraith2.clocking = True # 밖에서 .을 사용해서 변수를 추가할 수 있음
 
+# if wraith2.clocking == True: # 존재하지 않는 변수를 물어보면 오류가 남 wraith1로 바꾸면 wraith1에는 clocking이 없기 때문에 오류가 남
+#     print("{0}는 현재 클로킹 상태입니다".format(wraith2.name))
 
+# 메소드 & 상속 & 다중상속 & 메소드 오버라이딩
+# # 일반유닛
+# class Unit:
+#     def __init__(self, name, hp, speed): # __init__함수를 통해 파라미터를 받아옴
+#         self.name = name
+#         self.hp = hp
+#         self.speed = speed
+#         # self.damage = damage
+#         # print("{0} 유닛이 생성 되었습니다.".format(self.name))
+#         # print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+#     def move(self, location):
+#         print("[지상 유닛 이동]")
+#         print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(self.name, location, self.speed))
 
+# # 공격유닛
+# class AttackUnit(Unit): # 상속받으려는 클래스를 ()안에 쓰면 상속이 됨
+#     def __init__(self, name, hp, speed, damage):
+#         # self.name = name
+#         # self.hp = hp
+#         Unit.__init__(self, name, hp, speed)
+#         self.damage = damage
 
+#     def attack(self, location):
+#         print("{0} : {1} 방향으로 적군을 공격합니다. [공격력{2}]".format(self.name, location, self.damage))
 
+#     def damaged(self, damage):
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은 {1}입니다.".format(self.name, self.hp))
+#         if self.hp <= 0:
+#             print("{0} : 파괴되었습니다.".format(self.name))
 
+# # firebat1 = AttackUnit("파이어뱃", 50, 16)
+# # firebat1.attack("5시")
 
+# # firebat1.damaged(25)
+# # firebat1.damaged(25)
 
+# class Flyable:
+#     def __init__(self, flying_speed):
+#         self.flying_speed = flying_speed
+    
+#     def fly(self, name, location):
+#         print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+#             .format(name, location, self.flying_speed))
 
+# class FlyableAttackUnit(AttackUnit, Flyable):
+#     def __init__(self, name, hp, damage, flying_speed):
+#         AttackUnit.__init__(self, name, hp, 0, damage)
+#         Flyable.__init__(self, flying_speed)
 
+#     def move(self, location):
+#         print("[공중 유닛 이동]")
+#         self.fly(self.name, location)
 
+# # valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# # valkyrie.fly(valkyrie.name, "3시")
 
+# vulture = AttackUnit("벌쳐", 80, 10, 20)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+# vulture.move("11시")
+# battlecruiser.move("9시")
+'''210121'''
 
 
 
