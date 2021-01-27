6283efa1fdd0bcd785e214ac23b5ed5b3c5235af
kimchang0 <newshfkfk@gmail.com>
2021-01-27 22:10:34 +0900

    210127_study

--- practice.py
+++ practice.py
@@ -909,62 +909,62 @@
 #     print("{0}는 현재 클로킹 상태입니다".format(wraith2.name))
 
 # 메소드 & 상속 & 다중상속 & 메소드 오버라이딩
-# # 일반유닛
-# class Unit:
-#     def __init__(self, name, hp, speed): # __init__함수를 통해 파라미터를 받아옴
-#         self.name = name
-#         self.hp = hp
-#         self.speed = speed
-#         # self.damage = damage
-#         # print("{0} 유닛이 생성 되었습니다.".format(self.name))
-#         # print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
-#     def move(self, location):
-#         print("[지상 유닛 이동]")
-#         print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(self.name, location, self.speed))
+# 일반유닛
+class Unit:
+    def __init__(self, name, hp, speed): # __init__함수를 통해 파라미터를 받아옴
+        self.name = name
+        self.hp = hp
+        self.speed = speed
+        # self.damage = damage
+        # print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        # print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(self.name, location, self.speed))
 
-# # 공격유닛
-# class AttackUnit(Unit): # 상속받으려는 클래스를 ()안에 쓰면 상속됨
-#     def __init__(self, name, hp, speed, damage):
-#         # self.name = name
-#         # self.hp = hp
-#         Unit.__init__(self, name, hp, speed)
-#         self.damage = damage
+# 공격유닛
+class AttackUnit(Unit): # 상속받으려는 클래스를 ()안에 쓰면 상속됨
+    def __init__(self, name, hp, speed, damage):
+        # self.name = name
+        # self.hp = hp
+        Unit.__init__(self, name, hp, speed)
+        self.damage = damage
 
-#     def attack(self, location):
-#         print("{0} : {1} 방향으로 적군을 공격합니다. [공격력{2}]".format(self.name, location, self.damage))
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력{2}]".format(self.name, location, self.damage))
 
-#     def damaged(self, damage):
-#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
-#         self.hp -= damage
-#         print("{0} : 현재 체력은 {1}입니다.".format(self.name, self.hp))
-#         if self.hp <= 0:
-#             print("{0} : 파괴되었습니다.".format(self.name))
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1}입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
 
-# # firebat1 = AttackUnit("파이어뱃", 50, 16)
-# # firebat1.attack("5시")
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
 
-# # firebat1.damaged(25)
-# # firebat1.damaged(25)
+# firebat1.damaged(25)
+# firebat1.damaged(25)
 
-# class Flyable:
-#     def __init__(self, flying_speed):
-#         self.flying_speed = flying_speed
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
     
-#     def fly(self, name, location):
-#         print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
-#             .format(name, location, self.flying_speed))
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+            .format(name, location, self.flying_speed))
 
-# class FlyableAttackUnit(AttackUnit, Flyable):
-#     def __init__(self, name, hp, damage, flying_speed):
-#         AttackUnit.__init__(self, name, hp, 0, damage)
-#         Flyable.__init__(self, flying_speed)
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage)
+        Flyable.__init__(self, flying_speed)
 
-#     def move(self, location):
-#         print("[공중 유닛 이동]")
-#         self.fly(self.name, location)
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
 
-# # valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
-# # valkyrie.fly(valkyrie.name, "3시")
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# valkyrie.fly(valkyrie.name, "3시")
 
 # vulture = AttackUnit("벌쳐", 80, 10, 20)
 
@@ -973,7 +973,11 @@
 # battlecruiser.move("9시")
 '''210121'''
 
-
+class BuildingUnit(Unit):
+    def __init__(self, name, hp, location):
+        # Unit.__init__(self, name, hp, 0)
+        super().__init__(name, hp, 0)
+        self.location = location
 
 
 
