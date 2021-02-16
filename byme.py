1d5ee0d94a5065fedc5ef46a45545fbc8648b5c3
kimchang0 <newshfkfk@gmail.com>
2021-02-16 20:04:26 +0900

    210216_studyComplete

---
+++ byme.py
@@ -0,0 +1,5 @@
+def sign(people, email):
+    print("이 프로그램은 {0}에 의해 만들어졌습니다.".format(people))
+    print("유튜브 : http://youtube.com")
+    print("이메일 : {0}".format(email))
+
--- practice.py
+++ practice.py
@@ -1101,31 +1101,516 @@ class House:
         pass
 '''
 
-class House:
-    def __init__(self, location, house_type, deal_type, price, completion_year):
-        self.location = location
-        self.house_type = house_type
-        self.deal_type = deal_type
-        self.price = price
-        self.completion_year = completion_year
-        print("[알림] 부동산 정보가 추가되었습니다. {0} {1} {2} {3} {4}".format(location, house_type, deal_type, price, completion_year))
+# class House:
+#     def __init__(self, location, house_type, deal_type, price, completion_year):
+#         self.location = location
+#         self.house_type = house_type
+#         self.deal_type = deal_type
+#         self.price = price
+#         self.completion_year = completion_year
+#         print("[알림] 부동산 정보가 추가되었습니다. {0} {1} {2} {3} {4}".format(location, house_type, deal_type, price, completion_year))
 
-    def show_detail(self):
-        print("{0} {1} {2} {3} {4}".format(self.location, self.house_type, self.deal_type, self.price, self.completion_year))
+#     def show_detail(self):
+#         print("{0} {1} {2} {3} {4}".format(self.location, self.house_type, self.deal_type, self.price, self.completion_year))
 
-h1 = House("강남", "아파트", "매매", "10억", "2010년")
-h2 = House("마포", "오피스텔", "전세", "5억", "2007년")
-h3 = House("송파", "빌라", "월세", "500/50", "2000년")
+# h1 = House("강남", "아파트", "매매", "10억", "2010년")
+# h2 = House("마포", "오피스텔", "전세", "5억", "2007년")
+# h3 = House("송파", "빌라", "월세", "500/50", "2000년")
 
-houses = []
-houses.append(h1)
-houses.append(h2)
-houses.append(h3)
+# houses = []
+# houses.append(h1)
+# houses.append(h2)
+# houses.append(h3)
 
-print("총 {0}대의 매물이 있습니다.".format(len(houses)))
+# print("총 {0}대의 매물이 있습니다.".format(len(houses)))
 
-for house in houses:
-    house.show_detail()
+# for house in houses:
+#     house.show_detail()
+
+# 예외처리
+# try:
+#     print("나누기 전용 계산기입니다.")
+#     nums = []
+#     nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
+#     nums.append(int(input("두 번째 숫자를 입력하세요 : ")))
+#     nums.append(int(nums[0] / nums[1]))
+#     print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
+# except ValueError:
+#     print("에러! 잘못된 값을 입력하셨습니다.")
+# except ZeroDivisionError as err:
+#     print(err)
+# except Exception as err:
+#     print("알 수 없는 에러가 발생하였습니다.")
+#     print(err)
+
+# 에러 발생시키기 & 사용자 정의 예외처리 & finally
+# class BigNumberError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
+
+#     def __str__(self):
+#         return self.msg
+# try:
+#     print("한 자리 숫자 나누기 전용 계산기입니다.")
+#     num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     num2 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     if num1 >= 10 or num2 >= 10:
+#         raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
+#     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+# except BigNumberError as err:
+#     print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요.")
+#     print(err)
+# finally:
+#     print("계산기를 이용해 주셔서 감사합니다.")
+
+# 퀴즈
+'''Quiz) 동네에 항상 대기 손님이 있는 맛있는 치킨집이 있습니다.
+대기 손님의 치킨 요리 시간을 줄이고자 자동 주문 시스템을 제작하였습니다.
+시스템 코드를 확인하고 적절한 예외처리 구문을 넣으시오
+
+조건1 : 1보다 작거나 숫자가 아닌 입력값이 들어올 때는 ValueError 로 처리
+        출력 메시지 : "잘못된 값을 입력하였습니다."
+조건2 : 대기 손님이 주문할 수 있는 총 치킨량은 10마리로 한정
+        치킨 소진 시 사용자 정의 에러 [SoldOutError]를 발생시키고 프로그램 종료
+        출력 메시지 : "재고가 소진되어 더 이상 주문을 받지 않습니다."
+
+[코드]
+chicken = 10
+waiting = 1
+while(True):
+    print("[남은 치킨 : {0}]".format(chicken))
+    order = int(input("치킨 몇 마리 주문하시겠습니까?"))
+    if order > chicken:
+        print("재료가 부족합니다.")
+    else:
+        print("[대기번호 {0}] {1}마리 주문이 완료되었습니다.".foramt(waiting, order))
+        waiting += 1
+        chicken -= order
+'''
+# class SoldOutError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
+
+#     def __str__(self):
+#         return self.msg
+       
+# chicken = 20
+# waiting = 1
+# while(True):
+#     try:
+#         if chicken == 0:
+#             raise SoldOutError("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+#         else:
+#             print("[남은 치킨 : {0}]".format(chicken))
+#             order = 0
+#             if chicken >= 10:
+#                 print("[최대 주문 10마리 가능합니다.]")
+#                 order = int(input("치킨 몇 마리 주문하시겠습니까?"))
+#             elif chicken < 10:
+#                 print("[최대 주문 {0}마리 가능합니다.]".format(chicken))
+#                 order = int(input("치킨 몇 마리 주문하시겠습니까?"))
+
+#             if order > chicken:
+#                 print("주문 가능한 범위를 초과했습니다.")
+#             elif order > 10:
+#                 print("최대 10마리만 주문 가능합니다.")
+#             elif order < 1 or type(order) != int:
+#                 raise ValueError
+#             else:
+#                 print("[대기번호 {0}] {1}마리 주문이 완료되었습니다.".format(waiting, order))
+#                 waiting += 1
+#                 chicken -= order
+#     except ValueError as err:
+#         print("잘못된 값을 입력하셨습니다. 정수를 입력하세요.")
+#     except SoldOutError as err:
+#         print(err)
+#         break
+
+#모듈
+# import theater_module
+# theater_module.price(3) # 3명이서 영화 보러 갔을 때 가격
+# theater_module.price_morning(4) # 4명이서 조조 할인 영화 보러 갔을 때
+# theater_module.price_soldier(5) # 5명의 군인이 영화 보러 갔을 때
+
+# import theater_module as mv # mv라는 별칭을 지정해서 코드를 줄일 수 있음
+# mv.price(3)
+# mv.price_morning(4)
+# mv.price_soldier(5)
+
+# from theater_module import * # theater_module의 모든 함수를 사용하겠다고 선언해서 코드를 더 간단하게 만듦
+# price(3)
+# price_morning(4)
+# price_soldier(5)
+
+# from theater_module import price, price_morning # 내가 지정하는 함수만 가져오는 것도 가능함 대신 가져오지 않은 함수를 사용하면 오류가 남
+# price(5)
+# price_morning(6)
+
+# from theater_module import price_soldier as price # 함수에도 별칭을 지정하여 보다 간단하게 코드를 할 수 있음.
+# price(3)
+
+# 패키지(모듈들을 모아놓은 집합) & __all__ & 모듈 직접 실행
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPckage()
+# trip_to.detail()
+
+# from travel.thailand import ThailandPckage
+# trip_to = ThailandPckage()
+# trip_to.detail()
+
+# from travel import veitnam
+# trip_to = veitnam.VeitnamPackage()
+# trip_to.detail()
+
+# from travel import * # *을 썼을 때 어떤 것을 가져가게 할 것인지 정의해줘야 함. __init__파일에서 정의함.
+# trip_to = veitnam.VeitnamPackage() # 파일을 외부에서 실행할 수도 있고 내부에서 실행할 수도 있음.
+# trip_to = thailand.ThailandPckage()
+# trip_to.detail()
+
+# 패키지, 모듈 위치 inspect를 통해 파일 위치가 어디인지 알 수 있음. 패키지는 라이브러리 폴더에 옮겨도 사용할 수 있음.
+# import inspect
+# import random
+# from travel import *
+# print(inspect.getfile(random))
+# print(inspect.getfile(thailand))
+# trip_to = thailand.ThailandPckage()
+# trip_to.detail()
+
+# pip install
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+# print(soup.prettify())
+
+# 내장함수 : 기본적으로 탑재하고 있는 함수들 편리한 기능들이 많다.
+# language = input("무슨 언어를 좋아하세요?")
+# print("{0}은 아주 좋은 언어입니다!".format(language))
+
+# dir : 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+# print(dir())
+# import random # 외장 함수
+# print(dir())
+# import pickle
+# print(dir())
+# print(dir(random))
+
+# lst = [1, 2, 3]
+# print(dir(lst))
+
+# name = "Jim"
+# print(dir(name))
+
+# 외장함수 : 직접 import를 해서 사용해야 하는 함수 나한테 맞는 함수들을 찾아서 사용하면 된다.
+# glob : 경로 내의 폴더 / 파일 목록 조회 (윈도우 dir과 같은 기능)
+# import glob
+# print(glob.glob("*.py")) # 현재 workspace에서 확장자가 py인 모든 파일을 조회함
+
+# os : 운영체제에서 제공하는 기본 기능
+# import os
+# print(os.getcwd()) # 현재 디렉토리를 표시
+
+# folder = "sample_dir"
+
+# if os.path.exists(folder):
+#     print("이미 존재하는 폴더입니다.")
+#     os.rmdir(folder)
+#     print(folder, "폴더를 삭제하였습니다.")
+# else:
+#     os.makedirs(folder)
+#     print(folder, "폴더를 생성하였습니다.")
+# print(os.listdir()) # glob과 유사한 기능, 현재 폴더에 어떤 파일들이 있는지 보여준다.
+
+# time : 시간 관련 함수
+# import time
+# print(time.localtime())
+# print(time.strftime("%Y-%m-%d %H:%M:%S"))
+
+# import datetime
+# print("오늘 날짜는 ", datetime.date.today())
+# # timedelta : 두 날짜 사이의 간격
+# today = datetime.date.today() # 오늘 날짜 저장
+# td = datetime.timedelta(days=100)
+# print("우리가 만난지 100일은", today + td)
+
+'''Quiz) 프로젝트 내에 나만의 시그니처를 남기는 모듈을 만드시오.
+
+조건 모듈 파일명은 byme.py 로 작성
+
+(모듈 사용 예제)
+import byme
+byme.sign()
+
+(출력 예제)
+이 프로그램은 나도코딩에 의해 만들어졌습니다.
+유튜브 : http://youtube.com
+이메일 : nadocoding@gmail.com
+'''
+# import byme
+# byme.sign("이름", "이메일")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
